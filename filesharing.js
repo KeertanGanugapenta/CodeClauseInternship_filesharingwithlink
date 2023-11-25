997c99140ce2db41eb1209fd80176251b68be92d
KeertanGanugapenta <143919258+KeertanGanugapenta@users.noreply.github.com>
2023-11-25 10:30:41 +0530

    Add files via upload

---
+++ filesharing.js
@@ -0,0 +1,49 @@
+const fileInput = document.getElementById("fileInput");
+const uploadButton = document.getElementById("uploadButton");
+const shareableLink = document.getElementById("shareableLink");
+
+fileInput.addEventListener("change", () => {
+  const selectedFiles = fileInput.files;
+  if (selectedFiles.length > 0) {
+    const filenames = Array.from(selectedFiles)
+      .map((file) => file.name)
+      .join(", ");
+    const label = document.querySelector(".custom-file-lab");
+    label.innerHTML = filenames;
+  }
+});
+uploadButton.addEventListener("click", async () => {
+  const file = fileInput.files[0];
+  if (file) {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      uploadButton.disabled = true;
+      uploadButton.textContent = "Sharing.....";
+
+      const response = await fetch("https://file.io/?expires=1d", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      if (response.ok) {
+        const link = `<p>Download File <a href="${data.link}" target="_blank">${data.link}</a><p/>`;
+        shareableLink.innerHTML = link;
+      } else {
+        shareableLink.innerHTML = "File share failed. Please try again";
+      }
+    } catch (error) {
+      shareableLink.textContent = "An error occured. Please try again later.";
+      console.error("Error sharing file:", error);
+    } finally {
+      uploadButton.disabled = false;
+      uploadButton.textContent = "Share";
+    }
+  } else {
+    shareableLink.textContent = "Please upload a file to share.";
+  }
+});
